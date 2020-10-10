a8c64c224a0e14910baa45d94d25d84d70a2139d
Aarti <aarti.yogesh.jagdale@gmail.com>
2020-10-10 13:57:44 +0530

    update database

--- db/myusers_users.sql
+++ db/myusers_users.sql
@@ -25,12 +25,14 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `fullname` varchar(20) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `job` varchar(20) DEFAULT NULL,
-  `pwd` varchar(5) DEFAULT NULL,
-  `image` longtext
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `pwd` varchar(15) DEFAULT NULL,
+  `image` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +41,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('Atul','atul@gmail.com','SE','12234','qwdasdasdasdasdas'),('Mayur','ms@gmail.com','App','12234','qwdasdasdasdasdas'),('Komal','km@gmail.com','student','12234','image'),('q','qqqq@gmail.com','Teacher','11111',''),('aaaaaaaa','krishna@mail','web Devloper','12345','http://localhost:1100/creat-user2.html'),('Yogesh ','yog99mail@gmail.com','Software Engineer','8080','img/user3.jpg'),('bsjnj','yogesh@anattadesign.com','Student','1234','img/user3.jpg'),('Yogesh ','yogesh@anattadesign.com','Web Devloper','````','img/user3.jpg'),('Yogesh ','yogesh@anattadesign.com','Software Engineer','9090','img/user3.jpg'),('aaa','yog99mail@gmail.com','Web Devloper','1234','img/user3.jpg');
+INSERT INTO `users` VALUES (1,'aarti s','aarti.yogesh@gmail.com','Software Engineer','12344','img/user3.jpg'),(2,'aarti','aarti.yogesh@gmail.com','Web Devloper','3333544','img/user3.jpg'),(3,'aarti','yog99mail@gmail.com','Student','1234','img/user3.jpg'),(4,'aarti','yog99mail@gmail.com','Student','1222','img/user3.jpg'),(5,'Yogesh Jagdale','yogesh@anattadesign.com','Web Devloper','1222','img/user3.jpg'),(6,'aarti','yog99mail@gmail.com','Student','1111','img/user3.jpg'),(7,'aarti','yogesh@anattadesign.com','Web Devloper','23333','img/user3.jpg'),(8,'aarti','yog99mail@gmail.com','Student','1222','img/user3.jpg'),(9,'aarti','yogesh@anattadesign.com','Student','1233','img/user3.jpg'),(10,'Yogesh Jagdale','yog99mail@gmail.com','Student','23344','img/user3.jpg'),(11,'aarti','yog99mail@gmail.com','Student','12233','img/user3.jpg'),(12,'aarti','yog99mail@gmail.com','Web Devloper','12223','img/user3.jpg'),(13,'aarti','yog99mail@gmail.com','Student','11111','img/user3.jpg'),(14,'aarti','yog99mail@gmail.com','Student','2343456','img/user3.jpg'),(15,'Yogesh Jagdale','yog99mail@gmail.com','Web Devloper','12345','img/user3.jpg');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-27 15:46:10
+-- Dump completed on 2020-10-10 13:56:50
